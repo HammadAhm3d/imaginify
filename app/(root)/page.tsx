53934c62f744cbe60d434c31c4bf548ce97a8a9b
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2025-01-05 17:57:04 +0200

    clerk setup + layout + mongodb

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      Home
+      <UserButton />
+    </main>
+  );
+};
+
+export default Home;
